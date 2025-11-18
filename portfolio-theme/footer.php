4874c2ea18574b18bda094d87a75a2b2bacdae1b
EtherealPotatoes <82071510+EtherealPotatoes@users.noreply.github.com>
2025-11-18 09:03:18 -0800

    Deleted portfolio-theme.tar.gz

--- portfolio-theme/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> <?php echo esc_html(get_theme_mod('hero_title', 'Joseph Young')); ?>. All rights reserved.</p>
-    </div>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-</html>
